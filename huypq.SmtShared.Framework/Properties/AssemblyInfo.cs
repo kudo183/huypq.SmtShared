428396241583a654abf52a20a61a0a97b6bfb7f0
kudo183@gmail.com <kudo183@gmail.com>
2017-07-20 17:46:10 +0700

    - upgrade to VS 2017

--- huypq.SmtShared.Framework/Properties/AssemblyInfo.cs
+++ huypq.SmtShared.Framework/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("huypq.SmtShared.Test")]
+[assembly: AssemblyTitle("huypq.SmtShared.Framework")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("huypq.SmtShared.Test")]
+[assembly: AssemblyProduct("huypq.SmtShared.Framework")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8d59423a-9325-484d-a398-fedd48375155")]
+[assembly: Guid("dbb09141-9d0c-4867-9248-1fc2e8a5eec5")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
--- old.NET/huypq.SmtShared/huypq.SmtShared/Constant/SerializeType.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace huypq.SmtShared.Constant
-{
-    public class SerializeType
-    {
-        public const string Json = "json";
-        public const string Protobuf = "protobuf";
-    }
-}
