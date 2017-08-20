3da07fec1f657f5277fdca18e4e07b33e4feb2d8
kudo183@gmail.com <kudo183@gmail.com>
2017-08-20 22:27:55 +0700

    - change json only sesrialize marked property (same with proto-buf)

--- huypq.SmtShared/PagingResultDto.cs
+++ huypq.SmtShared/PagingResultDto.cs
@@ -2,17 +2,23 @@
 
 namespace huypq.SmtShared
 {
+    [Newtonsoft.Json.JsonObject(Newtonsoft.Json.MemberSerialization.OptIn)]
     [ProtoBuf.ProtoContract]
     public class PagingResultDto<T>
     {
+        [Newtonsoft.Json.JsonProperty]
         [ProtoBuf.ProtoMember(1)]
         public int TotalItemCount { get; set; }
+        [Newtonsoft.Json.JsonProperty]
         [ProtoBuf.ProtoMember(2)]
         public int PageIndex { get; set; }
+        [Newtonsoft.Json.JsonProperty]
         [ProtoBuf.ProtoMember(3)]
         public int PageCount { get; set; }
+        [Newtonsoft.Json.JsonProperty]
         [ProtoBuf.ProtoMember(4)]
         public List<T> Items { get; set; }
+        [Newtonsoft.Json.JsonProperty]
         [ProtoBuf.ProtoMember(5)]
         public long LastUpdateTime { get; set; }
 
