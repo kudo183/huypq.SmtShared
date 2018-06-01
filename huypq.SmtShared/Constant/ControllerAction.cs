fd3e7b84a08566de8ad106c5427ff9c980a69778
gaucon <kudo183@gmail.com>
2018-06-01 15:47:12 +0700

    - add IP action.

--- huypq.SmtShared/Constant/ControllerAction.cs
+++ huypq.SmtShared/Constant/ControllerAction.cs
@@ -14,6 +14,7 @@
             public const string ChangePassword = "changepassword";
             public const string ResetPassword = "resetpassword";
             public const string Logout = "logout";
+            public const string IP = "ip";
         }
 
         public class SmtEntityBase
